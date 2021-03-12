b360c0d92542df756d37ff2272f3321c7e4071ee
turarka <77821414+turarka@users.noreply.github.com>
2021-03-12 12:03:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const { fstat } = require("fs");
+var http = require("http")
+var fs = require("fs")
+
+function serveStaticFile(res,path, contentType, responseCode){
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data){
+        if(err){
+            res.writeHead(500,{"Content-Type": "text/plain"})
+            res.end("500 - Internal error")
+        }
+        else {
+            res.writeHead(responseCode, {"Content-Type":contentType});
+            res.end(data);
+        }
+    })
+}
+
+http.createServer(function(req, res){
+    var path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch(path){
+        case "":
+           serveStaticFile(res,"/index.html", "text/html");
+           break;
+        case "/about":
+            serveStaticFile(res,"/about.html", "text/html");
+           break;
+        case "/img/cry.jpg":
+            serveStaticFile(res,"/img/cry.jpg", "image/jpeg");
+           break;
+        case "/img/welcome.jpg":
+            serveStaticFile(res,"/img/welcome.jpg", "image/jpeg");
+           break;
+        case "/img/about.jpg":
+            serveStaticFile(res,"/img/about.jpg", "image/jpeg");
+           break;
+        case "/style.css":
+            serveStaticFile(res,"/style.css", "text/css");
+           break;
+        case "/graduation":
+            serveStaticFile(res,"/img/gallery/graduation.jpg", "image/jpeg");
+           break;
+        case "/study":
+            serveStaticFile(res,"/img/gallery/study.jpg", "image/jpeg");
+           break;
+        case "/video":
+            serveStaticFile(res, "/video/students/memes.mp4", "video/mp4");
+            break;
+        default:
+            serveStaticFile(res, "/error.html", "text/html", 404);
+            break;
+
+    }
+}).listen(3000)
+
+console.log("Server is working");
